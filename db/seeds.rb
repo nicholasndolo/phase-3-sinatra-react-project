3cefd427167c02a97b415ebc2a12b2c06e4ec1c5
Nicholas <nicholas.nmalinda@gmail.com>
2022-09-12 06:47:11 +0300

    seeded db with sample order instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,14 @@ Supplier.create(name: "Lumie Smart Cloths", location: "Eldoret")
 Supplier.create(name: "Fresh Produce Supplies", location: "Kinangop")
 
 # #create sample order instances
-# Order.create(quantity: "20 bales", supplier_id: 1, product_id: 1)
-# Order.create(quantity: "200 packets", supplier_id: 1, product_id: 5)
-# Order.create(quantity: "25 sets", supplier_id: 2, product_id: 4)
-# Order.create(quantity: "15 sets", supplier_id: 3, product_id: 4)
-# Order.create(quantity: "50 boxes", supplier_id: 4, product_id: 2)
-# Order.create(quantity: "200 pairs", supplier_id: 5, product_id: 3)
-# Order.create(quantity: "100 cattons", supplier_id: 6, product_id: 6)
-# Order.create(quantity: "200 pairs", supplier_id: 2, product_id: 6)
+Order.create(quantity: "20 bales", supplier_id: 1, product_id: 1)
+Order.create(quantity: "200 packets", supplier_id: 1, product_id: 5)
+Order.create(quantity: "25 sets", supplier_id: 2, product_id: 4)
+Order.create(quantity: "15 sets", supplier_id: 3, product_id: 4)
+Order.create(quantity: "50 boxes", supplier_id: 4, product_id: 2)
+Order.create(quantity: "200 pairs", supplier_id: 5, product_id: 3)
+Order.create(quantity: "100 cattons", supplier_id: 6, product_id: 6)
+Order.create(quantity: "200 pairs", supplier_id: 2, product_id: 6)
 
 
 
