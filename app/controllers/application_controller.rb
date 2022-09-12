6495b8440f1b757279c4f82b65233eb56fcae20e
Nicholas <nicholas.nmalinda@gmail.com>
2022-09-12 07:29:16 +0300

    created a delete route for products

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,9 +36,9 @@ class ApplicationController < Sinatra::Base
       product.to_json
   end
 
-#   delete '/products/:id' do
-#     product = Product.find(params[:id])
-#     product.destroy
-#     product.to_json
-#   end
-# end
+  delete '/products/:id' do
+    product = Product.find(params[:id])
+    product.destroy
+    product.to_json
+  end
+end
